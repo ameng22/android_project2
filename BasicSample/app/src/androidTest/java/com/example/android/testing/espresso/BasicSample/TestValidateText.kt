3edcb6dc67e83e67ac4a61d819bbd00e8ae068a3
Amen George <amen1george2@gmail.com>
2024-04-04 00:47:06 -0400

    IntrumentedTesting for TextView in mainActivity
    IntrumentedTesting for String in TextView in mainActivity

---
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/TestValidateText.kt
@@ -0,0 +1,20 @@
+package com.example.android.testing.espresso.BasicSample
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TestValidateText {
+    
+
+}
